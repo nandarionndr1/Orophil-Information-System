699348f2009aa086bd881c18ce9781d2d1f7d9c1
nandarionndr1 <leebet16@gmail.com>
2018-08-30 02:45:29 +0800

    first comm

---
+++ src/orophils/Orophils.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package orophils;
+
+/**
+ *
+ * @author Leebet-PC
+ */
+public class Orophils {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MasterControl a = new MasterControl();
+        a.setVisible(true);
+    }
+    
+}
